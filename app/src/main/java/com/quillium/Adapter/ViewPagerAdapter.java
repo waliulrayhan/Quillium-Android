909e20d4f38e7cddda377b5f5df0e56e0ce111c0
nishatrhythm <66105148+nishatrhythm@users.noreply.github.com>
2024-01-10 04:24:20 +0600

    Notification (Half)

---
+++ app/src/main/java/com/quillium/Adapter/ViewPagerAdapter.java
@@ -0,0 +1,43 @@
+package com.quillium.Adapter;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+import com.quillium.Fragment.Notification2Fragment;
+import com.quillium.Fragment.RequestFragment;
+
+public class ViewPagerAdapter extends FragmentPagerAdapter {
+    public ViewPagerAdapter(@NonNull FragmentManager fm) {
+        super(fm);
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        switch (position) {
+            case 0: return new Notification2Fragment();
+            case 1: return new RequestFragment();
+            default: return new Notification2Fragment();
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return 2;
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        String title = null;
+        if (position == 0) {
+            title = "NOTIFICATION";
+        } else if (position == 1) {
+            title = "REQUEST";
+        }
+        return title;
+    }
+}
