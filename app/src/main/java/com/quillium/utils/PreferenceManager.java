62e3d2f95a6822143fab7e702a80b57fb6caa305
Md. Waliul Islam Rayhan <b190305034@cse.jnu.ac.bd>
2024-01-30 12:28:39 +0600

    Update PreferenceManager.java

--- app/src/main/java/com/quillium/utils/PreferenceManager.java
+++ app/src/main/java/com/quillium/utils/PreferenceManager.java
@@ -1,4 +1,50 @@
 package com.quillium.utils;
 
 public class PreferenceManager {
+    private String username;
+    private String email;
+    private int age;
+
+    // Constructor
+    public PreferenceManager(String username, String email, int age) {
+        this.username = username;
+        this.email = email;
+        this.age = age;
+    }
+
+    // Getters and setters
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    // Additional methods if needed
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "username='" + username + '\'' +
+                ", email='" + email + '\'' +
+                ", age=" + age +
+                '}';
+    }
 }
