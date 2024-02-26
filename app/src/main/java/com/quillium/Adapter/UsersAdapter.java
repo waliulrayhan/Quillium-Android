04fc3d8f4f0c82542e66076cb1c7a23c623de512
Md. Waliul Islam Rayhan <b190305034@cse.jnu.ac.bd>
2024-02-27 01:46:55 +0600

    ok

--- app/src/main/java/com/quillium/Adapter/UsersAdapter.java
+++ app/src/main/java/com/quillium/Adapter/UsersAdapter.java
@@ -3,16 +3,25 @@ package com.quillium.Adapter;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.util.Base64;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.quillium.Listeners.UserListener;
 import com.quillium.Model.User;
 import com.quillium.R;
 import com.quillium.databinding.ItemContainerUserBinding;
+import com.squareup.picasso.Picasso;
 
 import java.util.List;
 
@@ -59,13 +68,45 @@ public class UsersAdapter extends RecyclerView.Adapter<UsersAdapter.UserViewHold
 
         void setUserData(User user) {
             binding.textName.setText(user.name);
-            binding.textEmail.setText(user.email);
             if (user.image != null) {
                 binding.imageProfile.setImageBitmap(getUserImage(user.image));
             } else {
                 binding.imageProfile.setImageResource(R.drawable.man); // Assuming you have a default profile photo resource
             }
             binding.getRoot().setOnClickListener(v -> userListener.onUserClicked(user));
+
+            
+            // Initialize Firebase Auth and Database references
+            FirebaseAuth auth = FirebaseAuth.getInstance();
+            DatabaseReference userRef;
+
+            FirebaseUser currentUser = auth.getCurrentUser();
+            userRef = FirebaseDatabase.getInstance().getReference("users");
+
+            if (currentUser != null) {
+                String userId = currentUser.getUid();
+
+                userRef.child(userId).addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        if (dataSnapshot.exists()) {
+
+                            com.quillium.User user = dataSnapshot.getValue(com.quillium.User.class);
+
+                            String email = user.getDepartment();
+
+                            binding.textEmail.setText(email);
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+                        // Handle error
+                        Log.e("FirebaseData", "Error reading user data: " + databaseError.getMessage());
+                    }
+                });
+            }
+
         }
 
     }
